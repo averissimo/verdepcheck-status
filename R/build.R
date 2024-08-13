2a4e1cd2a55a6e3ca2a2c687247094a28c4c6088
André Veríssimo <211358+averissimo@users.noreply.github.com>
2024-08-13 12:23:17 +0200

    feat: convert to package and adds debugme

---
+++ R/build.R
@@ -0,0 +1,135 @@
+#' Make internal request to GitHub API
+#'
+#' @param ...
+#' @param query
+#' @param body
+#' @param endpoint
+#' @param owner
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' make_curl_req("repos", "insightsengineering", "teal", "actions", "workflows", "scheduled.yaml", "runs")
+make_curl_req <- function(
+    ...,
+    query = NULL,
+    body = NULL,
+    endpoint = "repos",
+    owner = "insightsengineering") {
+
+  dots <- rlang::list2(...)
+
+  url_path <- do.call(file.path, dots)
+
+  url <- if (!is.null(endpoint) && !is.null(owner)) {
+    sprintf("https://api.github.com/%s/%s", endpoint, owner)
+  } else if (!is.null(endpoint)) {
+    sprintf("https://api.github.com/%s", endpoint)
+  }
+
+  "!DEBUG query: `url`/`url_path`"
+
+  httr2::request(file.path(url, url_path)) |>
+    httr2::req_headers(
+      "Accept" = "application/vnd.github+json",
+      "Authorization" = sprintf("Bearer %s", Sys.getenv("GITHUB_PAT")),
+      "X-GitHub-Api-Version" = "2022-11-28"
+    )
+}
+
+
+#' Build package table information on jobs
+#'
+#' @param pkg_name name of package
+#' @param yaml_files yaml files to query
+#' @param workflows job workflows to query
+#' @param keep_regex regex to keep on jobs
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' build_pkg_table("teal")
+build_pkg_table <- function(
+    pkg_name,
+    yaml_files = c("scheduled.yaml", "scheduled.yml"),
+    workflows = c("min_cohort", "min_isolated", "max", "release"),
+    keep_regex = "Dependency Test - (.*) 🔢 .*"
+) {
+
+  for (workflow_name in yaml_files) {
+    res <- tryCatch({
+      make_curl_req(pkg_name, "actions", "workflows", workflow_name, "runs") |>
+        httr2::req_url_query(
+          branch = "main"
+        ) |>
+        httr2::req_perform() |>
+        httr2::resp_body_json()
+    }, error = function(e) {
+      return(NULL)
+    })
+    if (!is.null(res)) break
+  }
+
+  if (is.null(res) || length(res$workflow_runs) == 0L) {
+    return(tibble::tibble(
+      package = pkg_name,
+      name = workflows,
+      status = NA_character_,
+      url = NA_character_,
+      icon = NA_character_
+    )
+    )
+  }
+
+  "!DEBUG Retrieving `pkg_name`..."
+
+  "!DEBUG Get jobs for workflow `res$workflow_runs[[1]]$id`"
+
+  jobs <- list(jobs = list(), total_count = 1L, page = 1L)
+  while (jobs$page <= 10 && jobs$total_count > length(jobs$jobs)) {
+    "!DEBUG Get page `jobs$page` of jobs"
+    new_jobs <- make_curl_req(
+      pkg_name,
+      "actions",
+      "runs",
+      res$workflow_runs[[1]]$id,
+      "jobs"
+    ) |>
+      httr2::req_url_query(per_page = 10, page = jobs$page) |>
+      httr2::req_perform() |>
+      httr2::resp_body_json()
+
+    jobs$jobs <- c(jobs$jobs, new_jobs$jobs)
+    jobs$total_count <- new_jobs$total_count
+    jobs$page <- jobs$page + 1L
+  }
+
+  processed <- jobs$jobs |>
+    purrr::keep(~ grepl(keep_regex, .x$name)) |>
+    purrr::map(
+      ~ list(
+        package = pkg_name,
+        name = gsub(keep_regex, "\\1", .x$name),
+        status = .x$conclusion,
+        url = .x$html_url
+      )
+    ) |>
+    dplyr::bind_rows()
+
+  "!DEBUG Keeping `length(processed)`: `paste(processed$name, processed$status, sep = '=', collapse = ', ')`"
+
+  processed |>
+    dplyr::bind_rows() |>
+    dplyr::mutate(
+      icon = dplyr::case_when(
+        status == "success" ~ "✅",
+        status == "failure" ~ "❌",
+        status == "cancelled" ~ "🚫",
+        is.na(status) ~ "❓",
+        .default = "",
+      ),
+      status = dplyr::if_else(is.na(status), "unkown", status)
+    )
+}
